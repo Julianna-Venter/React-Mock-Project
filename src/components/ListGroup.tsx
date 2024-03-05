5504bf575128f8a8237920b1241080c1cca126ea
juliannaventer <juliannaventer@gmail.com>
2024-03-05 19:06:59 +0200

    feat: :sparkles: Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,26 +1,24 @@
-
 //a component cannot return multiple elements. It can only return a single element.
 function ListGroup() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston"];
 
-    const items = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-    ];
-        
-    
-    // <> <=> <Fragment>
-    //This is a built-in component that allows you to return multiple elements from a component, without adding extra nodes to the DOM.
-    return (
-        <> 
-            <h1>List Group</h1>
-            <ul className="list-group">
-                {items.map((item) => (
-                <li className="list-group-item" key={item} >{item}</li>
-                ))}
-            </ul>
-        </>
-    );
+  items = [];
+
+  // <> <=> <Fragment>
+  //This is a built-in component that allows you to return multiple elements from a component, without adding extra nodes to the DOM.
+  return (
+    <>
+      <h1>List Group</h1>
+      {items.length === 0 && <p>There are no items in the list.</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
